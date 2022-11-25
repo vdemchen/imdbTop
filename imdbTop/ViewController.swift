5d6d5c4aada9505cb63dbbae461224ccf0e28384
Vladyslav Demchenko <vdemchen@gmail.com>
2022-11-25 10:17:45 +0200

    Initial Commit

---
+++ imdbTop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imdbTop
+//
+//  Created by Vlada Kemskaya on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
